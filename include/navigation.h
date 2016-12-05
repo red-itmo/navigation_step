83d47a40af37f76bd6103704f5bbb41fce556a8a
Ram2301 <Nicko_Dema@protonmail.com>
2016-12-05 17:05:19 +0300

    reorganisation

---
+++ include/navigation.h
@@ -0,0 +1,23 @@
+/*
+ * Code reorganization for navigation step.
+ *
+ *  Created on: 12.05.2016
+ *       Email: Nicko_Dema@protonmail.com
+ *              ITMO University
+ *              Robotics Engineering Department
+ */
+
+#ifndef NAV_STEP_
+#define NAV_STEP_
+
+#include <ros/ros.h>
+#include <tf/tf.h>
+#include <geometry_msgs/Twist.h>
+#include <std_srvs/Empty.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/PointStamped.h>
+//???
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+#endif  /*NAV_STEP_*/
---
+++ src/neo-main.cpp
@@ -0,0 +1,8 @@
+/*
+ * Code reorganization for navigation step.
+ *
+ *  Created on: 12.05.2016
+ *       Email: Nicko_Dema@protonmail.com
+ *              ITMO University
+ *              Robotics Engineering Department
+ */
