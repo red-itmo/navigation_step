29e66a6a3e9a56676e8c1fcb77cc00d621dcef26
Nikolay Dema <Nicko_Dema@protonmail.com>
2016-12-08 09:14:38 +0300

    reorg_30%

--- include/navigation.h
+++ include/navigation.h
@@ -12,12 +12,36 @@
 
 #include <ros/ros.h>
 #include <tf/tf.h>
-#include <geometry_msgs/Twist.h>
 #include <std_srvs/Empty.h>
+#include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/PointStamped.h>
-//???
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+
+calss Navi
+{
+    public:
+        Navi();
+        ~Navi();
+        spin();
+
+    private:
+        ros::NodeHandle nh;
+
+        ros::Subscriber clked_pnt_sub;          //points from MoveBaseServer
+        //ros::Subscriber pnt_sub;                //points from any other nodes
+
+        //ACTIONLIB should used to communicate with CONTROL_NODE
+        // +++++++++++++
+        ros::ServiceServer start_srv;
+        bool start_callback (std_srvs::Empty::Request& req, std_srvs::Empty::Response&);
+        // +++++++++++++
+
+        void clkd_pnt_callback (const geometry_msgs::PointStamped::ConstPtr&);
+
+}
+
+
 
 #endif  /*NAV_STEP_*/
---
+++ src/navigation.cpp
@@ -0,0 +1,33 @@
+/*
+ * Code reorganization for navigation step.
+ *
+ *  Created on: 12.05.2016
+ *       Email: Nicko_Dema@protonmail.com
+ *              ITMO University
+ *              Robotics Engineering Department
+ */
+
+#include <navigation.h>
+
+Navi::Navi(): nh("~")
+{
+    clked_pnt_sub = nh.subscribe("clicked_point", 30, clkd_pnt_callback)
+    start_srv = nh.advertiseService("start", start_callback)
+}
+
+void Navi::clkd_pnt_callback (const geometry_msgs::PointStamped::ConstPtr& stamped);
+{
+    //----------------------
+}
+
+bool Navi::start_callback (std_srvs::Empty::Request& req, std_srvs::Empty::Response&);
+{
+    //---------------
+}
+
+
+Navi::~Navi()
+{
+    clked_pnt_sub.shutdown();
+    start_srv.shutdown();
+}
